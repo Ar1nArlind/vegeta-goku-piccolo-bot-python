0a92e6f32382c45dc00ff233e84d3586aa5fd095
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 17:46:54 +0100

    Add files via upload

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,22 +1,17 @@
-import os
 import discord
 from discord.ext import commands
 
-class Fun(commands.Cog):
+client = discord.Client()
 
+class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
-    #events 
-@client.Cog.listener()
-async def on_ready(self):
-    print(f'Logged in as: {client.user.name}')
-    print(f"User ID: {client.user.id}")
-    print('-----')
 
-    #commands
-@commands.command()
- async def ping(self, ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+    @commands.command()
+    async def ping(self, ctx): # This was inside '__init__' before
+        await ctx.send(f'pong!\n{round(client.latency * 1000)}ms') 
+        return
 
 def setup(client):
     client.add_cog(Fun(client))
