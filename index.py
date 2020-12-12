1fb69afc29dd458a019255d26a307921c2deb6f3
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 23:17:04 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -17,22 +17,14 @@ async def ping(ctx):
 @bot.command()
 @commands.is_owner()
 async def load(ctx, extension):
-    try:
-        bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f'Cog loaded')
-    except discord.ext.commands.errors.NotOwner:
-        await ctx.send("Bot Owner only")
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Cog Loaded')
 
 @bot.command()
 @commands.is_owner()
 async def unload(ctx, extension):
-    try:
-        bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(f'Cog Unloaded')
-    except discord.ext.commands.errors.NotOwner:
-        await ctx.send("Bot Owner only")
-        
-            
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Cog Unloaded')    
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
