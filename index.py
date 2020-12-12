77d540372898e874268fb4815f8f1fb550a6d758
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 18:10:34 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,30 +1,18 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
 
 client = commands.Bot(command_prefix = '$')
 client = discord.Client()
 
 @client.event
-@asyncio.coroutine
- def on_ready():
+async def on_ready():
     print(f'Logged in as: {client.user.name}')
     print(f"User ID: {client.user.id}")
     print('-----')
 
 @client.command()
-    @asyncio.coroutine
-    def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-@client.command()
-    @asyncio.coroutine
-    def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-for filemame in os.listdir(./cogs):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-client.run('os.environ['DISCORD_TOKEN']')
+client.run(os.environ['DISCORD_TOKEN'])
