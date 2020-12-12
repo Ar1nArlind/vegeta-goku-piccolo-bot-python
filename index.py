9539e0d766f484b9821d729df09aa6a862db532a
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 17:31:34 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
+client = commands.Bot(command_prefix = '$')
 client = discord.Client()
 
 @client.event
@@ -10,4 +11,16 @@ async def on_ready():
     print(f"User ID: {client.user.id}")
     print('-----')
 
-client.run(os.environ['DISCORD_TOKEN'])
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+    @client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filemame in os.listdir(./cogs):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run('os.environ['DISCORD_TOKEN']')
