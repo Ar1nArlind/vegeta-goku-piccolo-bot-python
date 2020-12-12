da79d01fd931d241bf232b74c87363f83f0a7b82
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 18:00:13 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,26 @@
 import os
 import discord
 from discord.ext import commands
+import asyncio
 
 client = commands.Bot(command_prefix = '$')
 client = discord.Client()
 
 @client.event
-async def on_ready():
+@asyncio.coroutine
+ def on_ready():
     print(f'Logged in as: {client.user.name}')
     print(f"User ID: {client.user.id}")
     print('-----')
 
 @client.command()
-async def load(ctx, extension):
+    @asyncio.coroutine
+    def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
-    @client.command()
-async def unload(ctx, extension):
+@client.command()
+    @asyncio.coroutine
+    def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
 for filemame in os.listdir(./cogs):
