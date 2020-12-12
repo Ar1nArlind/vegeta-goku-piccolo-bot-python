cd25b0a1ac5c47045bda84f9e931c39d7119e849
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 18:24:34 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,12 +2,16 @@ import os
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '$')
-client = discord.Client()
+bot = commands.Bot(command_prefix = '$')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as: {client.user.name}')
-    print(f"User ID: {client.user.id}")
+    print(f'Logged in as: {bot.user.name}')
+    print(f"User ID: {bot.user.id}")
+    print('-----')
 
-client.run(os.environ['DISCORD_TOKEN'])
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+bot.run(os.environ['DISCORD_TOKEN'])
