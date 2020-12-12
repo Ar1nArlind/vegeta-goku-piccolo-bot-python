2d47085a74b9d55d8ae21917b869b56726da2347
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 18:18:39 +0100

    Update index.py

--- index.py
+++ index.py
@@ -10,8 +10,4 @@ async def on_ready():
     print(f'Logged in as: {client.user.name}')
     print(f"User ID: {client.user.id}")
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
-
 client.run(os.environ['DISCORD_TOKEN'])
