b43a919185d05dce0f29a7167e22888e762dbdaa
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-12 18:54:02 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -11,7 +11,17 @@ async def on_ready():
     print('-----')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f'ok loaded')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'ok unloaded')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run(os.environ['DISCORD_TOKEN'])
