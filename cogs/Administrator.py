ab11ce4fd56cce4d2ad5d94487c264503aca4f44
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-13 21:24:23 +0100

    Add files via upload

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -6,39 +6,36 @@ class Administrator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command() 
-    @commands.has_permissions(ban_members=True) 
-    async def ban(self, ctx, user: discord.Member, *, reason):
-        await ctx.guild.ban(user, reason=reason) 
-        await user.send(f"You have been banned in {ctx.guild} for {reason}")
-        await ctx.send(f"{user} has been successfully banned.") 
+    @commands.command()
+    async def ban(self, ctx, user: discord.User):
+        guild = ctx.guild
+        embed = discord.Embed(
+                title = 'Success',
+                descrition = f"{user} has been banned."
+        )
+        if ctx.author.guild_permission.ban_members:
+            await ctx.send(embed=embed)
+            await guild.ban(user=user)
+         
 
-    @commands.command() 
-    @commands.has_permissions(kick_members=True) 
-    async def kick(self, ctx, user: discord.Member, *, reason):
-        await ctx.guild.kick(user, reason=reason) 
-        await user.send(f"You have been kicked in {ctx.guild} for {reason}")
-        await ctx.send(f"{user} has been successfully kicked.") 
+    @commands.command()
+    async def kick(self, ctx, user: discord.User):
+        guild = ctx.guild
+        embed = discord.Embed(
+                title = 'Success',
+                descrition = f"{user} has been kicked."
+        )
+        if ctx.author.guild_permission.kick_members:
+            await ctx.send(embed=embed)
+            await guild.kick(user=user)
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount)
         await ctx.send(f"ok")
-
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, *, member):
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
-
-        for ban_entry in banned_users:
-            user = ban_entry.user
-
-            if (user.name, user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(id)
-                await ctx.send(f'Unbanned {user.mention}')
-                return
+        
+        
 
 def setup(bot):
     bot.add_cog(Administrator(bot))
