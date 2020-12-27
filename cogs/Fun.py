34017e408008f39ffaefd421f43f50b8a9aac1ab
Ar1nArlind <64758368+Ar1nArlind@users.noreply.github.com>
2020-12-27 01:16:14 +0100

    Add files via upload

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -18,5 +18,37 @@ class Fun(commands.Cog):
         embed = discord.Embed(title = "Swag Meter", description  = (text), color = (0xa11cff))
         await ctx.send(embed = embed)
 
+    @command(name="fact")
+	@cooldown(3, 60, BucketType.guild)
+	async def animal_fact(self, ctx, animal: str):
+		if (animal := animal.lower()) in ("dog", "cat", "panda", "fox", "bird", "koala"):
+			fact_url = f"https://some-random-api.ml/facts/{animal}"
+			image_url = f"https://some-random-api.ml/img/{'birb' if animal == 'bird' else animal}"
+
+			async with request("GET", image_url, headers={}) as response:
+				if response.status == 200:
+					data = await response.json()
+					image_link = data["link"]
+
+				else:
+					image_link = None
+
+			async with request("GET", fact_url, headers={}) as response:
+				if response.status == 200:
+					data = await response.json()
+
+					embed = Embed(title=f"{animal.title()} fact",
+								  description=data["fact"],
+								  colour=ctx.author.colour)
+					if image_link is not None:
+						embed.set_image(url=image_link)
+					await ctx.send(embed=embed)
+
+				else:
+					await ctx.send(f"API returned a {response.status} status.")
+
+		else:
+			await ctx.send("No facts are available for that animal.")
+
 def setup(bot):
     bot.add_cog(Fun(bot))
